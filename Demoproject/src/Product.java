9c17790121b13856f31c7fc5699b2d5038db1c78
rohit <rohit@LAPTOP-JTEV75P6>
2021-05-07 14:45:48 +0530

    This is my 2nd commit for creating Product class.

---
+++ Demoproject/src/Product.java
@@ -0,0 +1,19 @@
+
+public class Product {
+
+private int productId;
+public int getProductId() {
+	return productId;
+}
+public void setProductId(int productId) {
+	this.productId = productId;
+}
+public String getProductName() {
+	return productName;
+}
+public void setProductName(String productName) {
+	this.productName = productName;
+}
+private String productName;
+
+}
